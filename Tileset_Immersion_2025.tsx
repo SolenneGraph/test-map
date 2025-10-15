73ee4348a01007c3f449b234cd361481f52bd951
SolenneGraph <72999662+SolenneGraph@users.noreply.github.com>
2025-10-15 17:37:58 +0200

    supp tsx

--- Tileset_Immersion_2025.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="Tileset_Immersion_2025" tilewidth="32" tileheight="32" tilecount="6750" columns="75">
- <image source="tilesets/Tileset_Immersion_2025.png" width="2400" height="2880"/>
-</tileset>
